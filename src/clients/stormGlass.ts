33a8018097e57988df36456348a4c41ab6e7ac4b
Isaque Pestana <isaque.pestana@portaldedocumentos.com.br>
2020-11-11 13:50:37 -0300

    Adicionados generics para evitar que o axios fique exposto na implementação das regras

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,5 @@
 import { InternalError } from '@src/util/errors/internal-error';
-import { AxiosStatic } from 'axios';
+import * as HTTPUtil from '@src/util/request';
 import config, { IConfig } from 'config';
 export interface StormGlassPointSource {
     [key: string]: number;
@@ -54,7 +54,7 @@ export class StormGlass {
 
     readonly stormGlassAPISource = 'noaa';
 
-    constructor(protected request: AxiosStatic) {}
+    constructor(protected request = new HTTPUtil.Request()) {}
 
     public async fetchPoints(
         lat: number,
@@ -76,7 +76,7 @@ export class StormGlass {
 
             return this.normalizeResponse(response.data);
         } catch (err) {
-            if (err.response && err.response.status) {
+            if (HTTPUtil.Request.isRequestError(err)) {
                 throw new StormGlassResponseError(
                     `Error: ${JSON.stringify(err.response.data)} Code: ${
                         err.response.status
